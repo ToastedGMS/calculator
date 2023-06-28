724e76fc38a58f6d51f140f9d57b674e32ac66cc
ToastedGMS <125387064+ToastedGMS@users.noreply.github.com>
2023-06-28 01:35:49 -0300

    added switch statement for main 'operate' function

---
+++ script.js
@@ -0,0 +1,22 @@
+function operate(num1, operator, num2) {
+    switch (operator){
+     case '+':{
+         return num1+num2;
+         break;
+     }
+     case '-':{
+          return num1-num2;
+          break;
+     }
+     case '*':{
+         return num1*num2;
+         break;
+     }
+     case '/':{
+         return num1/num2;
+         break;
+     }
+ }
+ }
+ 
+ 
