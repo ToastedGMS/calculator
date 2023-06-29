01a80e550147d7bf4fa75704a482289830d97af6
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-06-29 15:04:50 -0300

    added functionality to on-screen buttons

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
+// main 'operate' function (executes basic math)
+let result = 0;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 function operate(num1, operator, num2) {
+    console.log(num1, operator, num2)
     switch (operator){
      case '+':{
-         return num1+num2;
+         result = num1+num2;
+         return result;
          break;
      }
      case '-':{
-          return num1-num2;
+          result = num1-num2;
+          return result;
           break;
      }
      case '*':{
-         return num1*num2;
+         result = num1*num2;
+         return result;
          break;
      }
      case '/':{
-         return num1/num2;
+         result = num1/num2;
+         return result;
          break;
      }
  }
  }
- 
- 
+
+//  functions that populate display when buttons are pressed 
+const btns = document.querySelector('.btns');
+
+let displayVal = '';
+let numVal = '';
+
+btns.addEventListener('click', function(e){
+    if ((e.target).className === 'numBtn'){
+        numVal += (e.target).textContent;
+        displayVal += (e.target).textContent;
+        const displayDiv = document.querySelector('.display');
+        displayDiv.innerHTML = displayVal;
+    } else if ((e.target).className === 'operator'){
+        operator += `${(e.target).textContent}`;
+        displayVal += (e.target).textContent;
+        const displayDiv = document.querySelector('.display');
+        displayDiv.innerHTML = displayVal;
+        if (num1 === 0){
+            num1 = parseInt(numVal);
+            numVal = '';
+        }else {
+            num2 = parseInt(numVal);
+            numVal = '';
+        }
+    } else if ((e.target).id === 'equals') {
+        num2 = parseInt(numVal);
+        numVal = '';
+        operate(num1, operator, num2)
+        const displayDiv = document.querySelector('.display');
+        displayDiv.innerHTML = result;
+    }
+})
