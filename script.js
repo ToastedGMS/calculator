8145b707152f927a8fc2bb425381cb6e835e9754
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-07-13 15:14:14 -0300

    added operation concatenation and decimals

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ function operate(num1, operator, num2) {
     console.log(num1, operator, num2)
     switch (operator){
      case '+':{
-         result = num1+num2;
+         result = (num1+num2).toFixed(2);
          return result;
          break;
      }
      case '-':{
-          result = num1-num2;
+          result = (num1-num2).toFixed(2);
           return result;
           break;
      }
      case '*':{
-         result = num1*num2;
+         result = (num1*num2).toFixed(2);
          return result;
          break;
      }
      case '/':{
-         result = num1/num2;
+         result = (num1/num2).toFixed(2);
          return result;
          break;
      }
@@ -36,28 +36,45 @@ let displayVal = '';
 let numVal = '';
 
 btns.addEventListener('click', function(e){
+    
+    function display(){
+        displayVal += (e.target).textContent;
+        const displayDiv = document.querySelector('.display');
+        displayDiv.innerHTML = displayVal;}
+    
     if ((e.target).className === 'numBtn'){
         numVal += (e.target).textContent;
-        displayVal += (e.target).textContent;
-        const displayDiv = document.querySelector('.display');
-        displayDiv.innerHTML = displayVal;
+        display();
     } else if ((e.target).className === 'operator'){
+        if (operator === ''){
         operator += `${(e.target).textContent}`;
-        displayVal += (e.target).textContent;
-        const displayDiv = document.querySelector('.display');
-        displayDiv.innerHTML = displayVal;
-        if (num1 === 0){
-            num1 = parseInt(numVal);
+        display();
+        num1 = parseFloat(numVal);
+        numVal = '';
+        } else if (operator != ''){
+            num2 = parseFloat(numVal);
             numVal = '';
-        }else {
-            num2 = parseInt(numVal);
-            numVal = '';
-        }
+            operate(num1, operator, num2);
+            operator = '';
+            num1 = parseFloat(result);
+            displayVal = '';
+            const displayDiv = document.querySelector('.display');
+            displayDiv.innerHTML = num1;
+            operator += `${(e.target).textContent}`;
+            displayDiv.innerHTML += `${(e.target).textContent}`;}
     } else if ((e.target).id === 'equals') {
-        num2 = parseInt(numVal);
+        num2 = parseFloat(numVal);
         numVal = '';
         operate(num1, operator, num2)
         const displayDiv = document.querySelector('.display');
         displayDiv.innerHTML = result;
+    } else if ((e.target).id === 'clear') {
+        window.location.reload();
+    } else if ((e.target).id === 'decimal') {
+        if (numVal.includes(`${(e.target).textContent}`)){
+            alert('Please type only one decimal');
+        } else {
+            numVal += `${(e.target).textContent}`;
+            display();}
     }
 })
